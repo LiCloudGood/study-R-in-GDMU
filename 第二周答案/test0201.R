069ead3aef8f48f8078ea339027bbc52c65b831a
FrostLeaf <Li_CloudGood@outlook.com>
2025-04-04 19:28:07 +0800

    Add files via upload

---
+++ 第二周答案/test0201.R
@@ -0,0 +1,23 @@
+
+vector1 <- 1:10
+vector2 <- c("apple", "banana", "cherry")
+vector3 <- rep(1:5, times = 2)
+vector4 <- seq(from = 10, to = 50, by = 10)
+
+
+print(vector1)
+print(vector2)
+print(vector3)
+print(vector4)
+
+
+print(length(vector1))
+print(length(vector2))
+
+print(vector1[3:7])
+
+print(c(vector1, vector2))
+
+print(rep(c(-5, 3, 7), times = c(3, 4, 2)))
+
+print(seq(from = 1, to = 10, by = 2))
