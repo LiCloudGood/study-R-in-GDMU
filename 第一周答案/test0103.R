8f145823430329591709814dfa03e1a6f3d95541
FrostLeaf <Li_CloudGood@outlook.com>
2025-04-04 17:19:46 +0800

    Add files via upload

---
+++ 第一周答案/test0103.R
@@ -0,0 +1,8 @@
+setwd("e:/test01")
+install.packages("vioplot_0.4.0.tar.gz",repos = NULL)
+library(viopolt)
+vioplot(mtcars$wt[mtcars$cyl==4],
+        mtcars$wt[mtcars$cyl==6],
+        mtcars$wt[mtcars$cyl==8],
+        horizontal = TRUE,
+        col = 'red')
